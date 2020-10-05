c72817177def5d950d3ae2fe32734ed66fdecde8
Geoff Schmit <gcschmit@gmail.com>
2020-10-05 13:48:41 -0500

    Initial commit

---
+++ MileageTracker.java
@@ -0,0 +1,73 @@
+
+/**
+ * This class models a mileage tracker for a car.
+ *
+ * @author gcschmit
+ * @version 27 September 2020
+ */
+public class MileageTracker
+{
+    
+    
+    
+    
+    
+    
+    
+    /**
+     * Increment the number of gallons of fuel that this car has consumed
+     * 
+     * @param gallons    the additional fuel, in gallons, this car has consumed
+     */
+    public void incrementFuelConsumed(double gallons)
+    {
+        
+    }
+    
+    /**
+     * Returns the total number of gallons of fuel consumed
+     * 
+     * @return the total number of gallons of fuel consumed
+     */
+    public double getFuelConsumed()
+    {
+        return 0.0;
+    }
+    
+    
+    /**
+     * Returns the current mileage, in miles per gallon
+     * 
+     * @return the current mileage, in miles per gallon
+     */
+    public double getMileage()
+    {
+        return 0.0;
+    }
+    
+    /**
+     * Returns the vehicle identification (VIN) of this car.
+     * 
+     * @return the vehicle identification (VIN) of this car
+     */
+    public String getVIN()
+    {
+        return null;
+    }
+    
+    /**
+     * Sets the vehicle identification (VIN) of this car.
+     * 
+     * @param newVIN    the vehicle identification (VIN) of this car
+     */
+    public void setVIN(String vin)
+    {
+        vin = vin;
+    }
+}
+
+
+
+
+
+
