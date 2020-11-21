fdfa92bea1d3e401c3a22ba8f3807210c56cc224
Ria Pande <rpande@stu.naperville203.org>
2020-11-21 16:46:13 -0600

    added arrrow from window class to building class instead both of them directly connecting to CityscapeComponent put windows on all buildings

--- Building.java
+++ Building.java
@@ -19,6 +19,60 @@ public class Building
     private int buildingWidth;
     private int xLeft;
     private int yTop;
+    private Window window1;
+    private Window window2;
+    private Window window3;
+    private Window window4;
+    private Window window5;
+    private Window window6;
+    private Window window7;
+    private Window window8;
+    private Window window9;
+    private Window window10;
+    private Window window11;
+    private Window window12;
+    private Window window13;
+    private Window window14;
+    private Window window15;
+    private Window window16;
+    private Window window17;
+    private Window window18;
+    private Window window19;
+    private Window window20;
+    private Window window21;
+    private Window window22;
+    private Window window23;
+    private Window window24;
+    private Window window25;
+    private Window window26;
+    private Window window27;
+    private Window window28;
+    private Window window29;
+    private Window window30;
+    private Window window31;
+    private Window window32;
+    private Window window33;
+    private Window window34;
+    private Window window35;
+    private Window window36;
+    private Window window37;
+    private Window window38;
+    private Window window39;
+    private Window window40;
+    private Window window41;
+    private Window window42;
+    private Window window43;
+    private Window window44;
+    private Window window45;
+    private Window window46;
+    private Window window47;
+    private Window window48;
+    private Window window49;
+    private Window window50;
+    private Window window51;
+    private Window window52;
+    private Window window53;
+    private Window window54;
 
     /**
     Constructs a target.
@@ -40,9 +94,141 @@ public class Building
     {   
         Rectangle2D.Double building = 
         new Rectangle2D.Double(this.xLeft, this.yTop, this.buildingWidth, this.buildingLength);
-
+        
+        g2.setPaint(Color.black);
         g2.fill(building);
         
-        
+        if (buildingLength >= 100)
+        {
+            Window window1 = new Window(xLeft + 5,612);
+            Window window2 = new Window(xLeft + 39,612);
+            Window window3 = new Window(xLeft + 73,612);
+            Window window4 = new Window(xLeft + 5,646);
+            Window window5 = new Window(xLeft + 39,646);
+            Window window6 = new Window(xLeft + 73,646);
+            Window window7 = new Window(xLeft + 5,680);
+            Window window8 = new Window(xLeft + 39,680);
+            Window window9 = new Window(xLeft + 73,680);
+            
+            window1.draw(g2);
+            window2.draw(g2);
+            window3.draw(g2);
+            window4.draw(g2);
+            window5.draw(g2);
+            window6.draw(g2);
+            window7.draw(g2);
+            window8.draw(g2);
+            window9.draw(g2);
+        }
+        if (buildingLength >= 200)
+        {
+            Window window10 = new Window(xLeft + 5,578);
+            Window window11 = new Window(xLeft + 39,578);
+            Window window12 = new Window(xLeft + 73,578);
+            Window window13 = new Window(xLeft + 5,544);
+            Window window14 = new Window(xLeft + 39,544);
+            Window window15 = new Window(xLeft + 73,544);
+            Window window16 = new Window(xLeft + 5,510);
+            Window window17 = new Window(xLeft + 39,510);
+            Window window18 = new Window(xLeft + 73,510);
+            
+            window10.draw(g2);
+            window11.draw(g2);
+            window12.draw(g2);
+            window13.draw(g2);
+            window14.draw(g2);
+            window15.draw(g2);
+            window16.draw(g2);
+            window17.draw(g2);
+            window18.draw(g2);
+        }
+        if (buildingLength >= 300)
+        {  
+            Window window19 = new Window(xLeft + 5,476);
+            Window window20 = new Window(xLeft + 39,476);
+            Window window21 = new Window(xLeft + 73,476);
+            Window window22 = new Window(xLeft + 39,442);
+            Window window23 = new Window(xLeft + 5,442);
+            Window window24 = new Window(xLeft + 73,442);
+            Window window25 = new Window(xLeft + 73,408);
+            Window window26 = new Window(xLeft + 39,408);
+            Window window27 = new Window(xLeft + 5,408);
+            
+            window19.draw(g2);
+            window20.draw(g2);
+            window21.draw(g2);
+            window22.draw(g2);
+            window23.draw(g2);
+            window24.draw(g2);
+            window25.draw(g2);
+            window26.draw(g2);
+            window27.draw(g2);
+        }
+        if (buildingLength >= 400)
+        {
+            Window window28 = new Window(xLeft + 5,374);
+            Window window29 = new Window(xLeft + 39,374);
+            Window window30 = new Window(xLeft + 73,374);
+            Window window31 = new Window(xLeft + 39,340);
+            Window window32 = new Window(xLeft + 5,340);
+            Window window33 = new Window(xLeft + 73,340);
+            Window window34 = new Window(xLeft + 73,306);
+            Window window35 = new Window(xLeft + 39,306);
+            Window window36 = new Window(xLeft + 5,306);
+            
+            window28.draw(g2);
+            window29.draw(g2);
+            window30.draw(g2);
+            window31.draw(g2);
+            window32.draw(g2);
+            window33.draw(g2);
+            window34.draw(g2);
+            window35.draw(g2);
+            window36.draw(g2);
+        }
+        if (buildingLength >= 500)
+        {
+            Window window37 = new Window(xLeft + 5,272);
+            Window window38 = new Window(xLeft + 39,272);
+            Window window39 = new Window(xLeft + 73,272);
+            Window window40 = new Window(xLeft + 39,238);
+            Window window41 = new Window(xLeft + 5,238);
+            Window window42 = new Window(xLeft + 73,238);
+            Window window43 = new Window(xLeft + 73,204);
+            Window window44 = new Window(xLeft + 39,204);
+            Window window45 = new Window(xLeft + 5,204);
+            
+            window37.draw(g2);
+            window38.draw(g2);
+            window39.draw(g2);
+            window40.draw(g2);
+            window41.draw(g2);
+            window42.draw(g2);
+            window43.draw(g2);
+            window44.draw(g2);
+            window45.draw(g2);
+        }
+        if (buildingLength == 600)
+        {
+            Window window46 = new Window(xLeft + 5,170);
+            Window window47 = new Window(xLeft + 39,170);
+            Window window48 = new Window(xLeft + 73,170);
+            Window window49 = new Window(xLeft + 39,136);
+            Window window50 = new Window(xLeft + 5,136);
+            Window window51 = new Window(xLeft + 73,136);
+            Window window52 = new Window(xLeft + 73,102);
+            Window window53 = new Window(xLeft + 39,102);
+            Window window54 = new Window(xLeft + 5,102);
+            
+            window46.draw(g2);
+            window47.draw(g2);
+            window48.draw(g2);
+            window49.draw(g2);
+            window50.draw(g2);
+            window51.draw(g2);
+            window52.draw(g2);
+            window53.draw(g2);
+            window54.draw(g2);
+        }
     }
 }
--- CityscapeComponent.java
+++ CityscapeComponent.java
@@ -31,27 +31,26 @@ public class CityscapeComponent extends JComponent
   private Building building12; 
   private Building building13; 
   private Building building14; 
-  private Window window1;
     
     @Override
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;  // cast to Graphics2D object
 
-        Building building1 = new Building(200, 500, 100, 200);
-        Building building2 = new Building(400, 400, 100, 300);
-        Building building3 = new Building(600, 600, 100, 100);
-        Building building4 = new Building(800, 300, 100, 400);
-        Building building5 = new Building(301, 200, 98, 500);
-        Building building6 = new Building(501, 500, 98, 200);
-        Building building7 = new Building(701, 100, 98, 600);
-        Building building8 = new Building(901, 200, 98, 500);
-        Building building9 = new Building(0, 600, 100, 100);
-        Building building10 = new Building(101, 300, 98, 400);
-        Building building11 = new Building(1000, 600, 100, 100);
-        Building building12 = new Building(1101, 300, 98, 400);
-        Building building13 = new Building(1200, 200, 100, 500);
-        Building building14 = new Building(1301, 400, 98, 300);
+        Building building1 = new Building(200, 500, 99, 200);
+        Building building2 = new Building(400, 400, 99, 300);
+        Building building3 = new Building(600, 600, 99, 100);
+        Building building4 = new Building(800, 300, 99, 400);
+        Building building5 = new Building(300, 200, 99, 500);
+        Building building6 = new Building(500, 500, 99, 200);
+        Building building7 = new Building(700, 100, 99, 600);
+        Building building8 = new Building(900, 200, 99, 500);
+        Building building9 = new Building(0, 600, 99, 100);
+        Building building10 = new Building(100, 300, 99, 400);
+        Building building11 = new Building(1000, 600, 99, 100);
+        Building building12 = new Building(1100, 300, 99, 400);
+        Building building13 = new Building(1200, 200, 99, 500);
+        Building building14 = new Building(1300, 400, 99, 300);
 
         building1.draw(g2);
         building2.draw(g2);
@@ -69,118 +68,8 @@ public class CityscapeComponent extends JComponent
         building14.draw(g2);
         
         
-        Window window1 = new Window(105,510);
-        Window window2 = new Window(139,510);
-        Window window3 = new Window(173,510);
-        Window window4 = new Window(105,544);
-        Window window5 = new Window(139,544);
-        Window window6 = new Window(173,544);
-        Window window7 = new Window(105,578);
-        Window window8 = new Window(139,578);
-        Window window9 = new Window(173,578);
-        Window window10 = new Window(105,612);
-        Window window11 = new Window(139,612);
-        Window window12 = new Window(173,612);
-        Window window13 = new Window(105,646);
-        Window window14 = new Window(139,646);
-        Window window15 = new Window(173,646);
-        Window window16 = new Window(105,680);
-        Window window17 = new Window(139,680);
-        Window window18 = new Window(173,680);
-        Window window19 = new Window(105,476);
-        Window window22 = new Window(139,476);
-        Window window25 = new Window(173,476);
-        Window window20 = new Window(139,442);
-        Window window23 = new Window(105,442);
-        Window window26 = new Window(173,442);
-        Window window21 = new Window(173,408);
-        Window window24 = new Window(139,408);
-        Window window27 = new Window(105,408);
-        Window window28 = new Window(105,374);
-        Window window29 = new Window(139,374);
-        Window window30 = new Window(173,374);
-        Window window31 = new Window(139,340);
-        Window window32 = new Window(105,340);
-        Window window33 = new Window(173,340);
-        Window window34 = new Window(173,306);
-        Window window35 = new Window(139,306);
-        Window window36 = new Window(105,306);
         
-        Window window37 = new Window(206,680);
-        Window window38 = new Window(240,680);
-        Window window39 = new Window(274,680);
-        Window window40 = new Window(240,646);
-        Window window41 = new Window(206,646);
-        Window window42 = new Window(274,646);
-        Window window43 = new Window(274,612);
-        Window window44 = new Window(240,612);
-        Window window45 = new Window(206,612);
-        Window window46 = new Window(206,578);
-        Window window47 = new Window(240,578);
-        Window window48 = new Window(274,578);
-        Window window49 = new Window(240,544);
-        Window window50 = new Window(206,544);
-        Window window51 = new Window(274,544);
-        Window window52 = new Window(274,510);
-        Window window53 = new Window(240,510);
-        Window window54 = new Window(206,510);
-        
-        
-        window1.draw(g2);
-        window2.draw(g2);
-        window3.draw(g2);
-        window4.draw(g2);
-        window5.draw(g2);
-        window6.draw(g2);
-        window7.draw(g2);
-        window8.draw(g2);
-        window9.draw(g2);
-        window10.draw(g2);
-        window11.draw(g2);
-        window12.draw(g2);
-        window13.draw(g2);
-        window14.draw(g2);
-        window15.draw(g2);
-        window16.draw(g2);
-        window17.draw(g2);
-        window18.draw(g2);
-        window19.draw(g2);
-        window20.draw(g2);
-        window21.draw(g2);
-        window22.draw(g2);
-        window23.draw(g2);
-        window24.draw(g2);
-        window25.draw(g2);
-        window26.draw(g2);
-        window27.draw(g2);
-        window28.draw(g2);
-        window29.draw(g2);
-        window30.draw(g2);
-        window31.draw(g2);
-        window32.draw(g2);
-        window33.draw(g2);
-        window34.draw(g2);
-        window35.draw(g2);
-        window36.draw(g2);
-        
-        window37.draw(g2);
-        window38.draw(g2);
-        window39.draw(g2);
-        window40.draw(g2);
-        window41.draw(g2);
-        window42.draw(g2);
-        window43.draw(g2);
-        window44.draw(g2);
-        window45.draw(g2);
-        window46.draw(g2);
-        window47.draw(g2);
-        window48.draw(g2);
-        window49.draw(g2);
-        window50.draw(g2);
-        window51.draw(g2);
-        window52.draw(g2);
-        window53.draw(g2);
-        window54.draw(g2);
+      
     }
 
     /**
